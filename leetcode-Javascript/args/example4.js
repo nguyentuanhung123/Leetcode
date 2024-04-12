a4b74a60b046c0199e5326657546f672328a874f
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-04-12 23:21:50 +0700

    edit example4.js

--- leetcode-Javascript/args/example4.js
+++ leetcode-Javascript/args/example4.js
@@ -10,7 +10,7 @@ const array1 = [{},null,42] // Array of arguments
 const array2 = [1, 2, 3] // Array of arguments
 const obj1 = {name: "Hung", age: 22} // Object of arguments
 
-console.log("Result: ",f(array1, array2, obj1)); // Result:  [ [ {}, null, 42 ] ]
+console.log("Result: ",f(array1, array2, obj1)); // Result:  [ [ {}, null, 42 ], [ 1, 2, 3 ], { name: 'Hung', age: 22 } ]
 console.log("Type of result: ", typeof(f(array1, array2, obj1))); // Type of result:  object
 
 /**
